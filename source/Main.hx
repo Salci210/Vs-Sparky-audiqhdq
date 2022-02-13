e0c9152d6da2f7b14c9b0e62b018feabe64d5350
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2022-02-12 23:59:09 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,11 +1,8 @@
 package;
 
-import lime.app.Application;
-#if windows
-import Discord.DiscordClient;
-#end
 import openfl.display.BlendMode;
 import openfl.text.TextFormat;
+import openfl.display.Application;
 import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.FlxGame;
@@ -15,11 +12,6 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
-#if mobileC
-import sys.FileSystem;
-import lime.system.System;
-import android.*;
-#end
 
 class Main extends Sprite
 {
@@ -27,18 +19,13 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 60; // How many frames per second the game should run at.
+	var framerate:Int = 120; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
-	public static var watermarks = true; // Whether to put Kade Engine literally anywhere
+	public static var watermarks = true; // Whether to put Kade Engine liteartly anywhere
 
-	// You can Vs tty much ignore everything from here on - your code should go in your states.
-
-        #if mobileC
-        private static var androidDir:String = null;
-        private static var storagePath:String = AndroidTools.getExternalStorageDirectory();  
-        #end
+	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function main():Void
 	{
@@ -64,23 +51,6 @@ class Main extends Sprite
 
 	public static var webmHandler:WebmHandler;
 
-        static public function getDataPath():String
-        {
-        	#if mobileC 
-                if (androidDir != null && androidDir.length > 0) 
-                {
-                        return androidDir;
-                } 
-                else 
-                { 
-                        androidDir = storagePath + "/" + Application.current.meta.get("packageName") + "/files/";
-                }
-                return androidDir;
-                #else
-                return "";
-	        #end
-        }
-
 	private function init(?E:Event):Void
 	{
 		if (hasEventListener(Event.ADDED_TO_STAGE))
@@ -105,51 +75,20 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-                #if mobileC 
-                if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
-		    AndroidTools.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
-		}  
-
-                var grantedPermsList:Array<Permissions> = AndroidTools.getGrantedPermissions();    
-
-                if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
-                	if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
-                        	Application.current.window.alert("si le diste a aceptar permisos piola podes continuar lol, but si no lo hicist pos jodet na mentira solo activalos en ajustes" + "\n" + "Dale a ok","Permissions");
-                                System.exit(0);//Will close the game
-		        } else {
-                        	Application.current.window.alert("Weon el mp4 nesecita permisos de almacenamiento coñ" + "\n" + "Fuck u","Permissions");
-                                System.exit(0);//Will close the game
-		        }
-                }
-                else
-                {
-                        if (!FileSystem.exists(storagePath + "/" + Application.current.meta.get("packageName"))) {
-                                FileSystem.createDirectory(storagePath + "/" + Application.current.meta.get("packageName"));
-                        } 
-                        if (!FileSystem.exists(storagePath + "/" + Application.current.meta.get("packageName") + '/files')) {
-                                FileSystem.createDirectory(storagePath + "/" + Application.current.meta.get("packageName") + '/files');
-                        }
-                        if (!FileSystem.exists(Main.getDataPath() + "assets")) {
-                                FileSystem.createDirectory(Main.getDataPath() + "assets");
-                        }
-                }
-                #end
-
+		#if cpp
+		initialState = TitleState;
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		addChild(game);
-
-		#if windows
-		DiscordClient.initialize();
-
-		Application.current.onExit.add (function (exitCode) {
-			DiscordClient.shutdown();
-		 });
-		 
+		#else
+		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 		#end
-
+		addChild(game);
+		
+		#if !mobile
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
+
+		#end
 	}
 
 	var game:FlxGame;
