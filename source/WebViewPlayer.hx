8cfbe5a06da015bdca8d158c5af8cd18c0aa972e
El pepe <85134096+Salci210@users.noreply.github.com>
2022-03-15 10:37:58 -0600

    update WebViewPlayer.hx

--- source/WebViewPlayer.hx
+++ source/WebViewPlayer.hx
@@ -21,7 +21,7 @@ class WebViewPlayer extends FlxBasic
 		WebView.onClose=onClose;
 		WebView.onURLChanging=onURLChanging;
 
-		WebView.open(androidPath + source + '.html', false, null, ['http://exitme(.*)']);
+		
 	}
 
 	function onClose() {
